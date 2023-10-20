242fc54fd37125394deb8c86042d90aacf687559
gorrion130 <valbornozmateos@gmail.com>
2023-10-20 16:08:19 +0200

    fix structure

--- src/main/java/org/conecta/Main.java
+++ src/main/java/org/conecta/Main.java
@@ -1,7 +1,8 @@
 package org.conecta;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args)
+    {
         System.out.println("Hello world!");
     }
 }
