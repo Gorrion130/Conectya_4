f292c44a926825ce25c3feb2416c8572efa1d8d2
gorrion130 <valbornozmateos@gmail.com>
2023-10-20 17:05:00 +0200

    more things added

--- src/main/java/com/project/Brain.java
+++ src/main/java/com/project/Brain.java
@@ -1,12 +1,19 @@
 package com.project;
 
 import java.net.URI;
+import java.net.URISyntaxException;
 
 import org.java_websocket.client.WebSocketClient;
 
 public class Brain {
     void brain(String ip, Integer port) {
-        WebSocketClient client = new Netw(new URI("ws://"+ip+":"+Integer.toString(port)));
-		client.connect();
+        WebSocketClient client;
+        try {
+            client = new Netw(new URI("ws://"+ip+":"+Integer.toString(port)));
+            client.connect();
+        } catch (URISyntaxException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
     }
 }
